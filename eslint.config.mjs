ba732624e49ce79af2e79d792c1493abdac3024b
Matheus Zucchi <matheusmzucchi@gmail.com>
2025-04-10 22:59:57 -0300

    Addded eslint to the project

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["airbnb-base"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+]);
