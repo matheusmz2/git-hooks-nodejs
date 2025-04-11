9f3bbb0b3d99c5990d7d879ac6ae677ed38828cf
Matheus Zucchi <matheusmzucchi@gmail.com>
2025-04-10 23:06:22 -0300

    Modifying eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import json from "@eslint/json";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["airbnb-base"] },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
